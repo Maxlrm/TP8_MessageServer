859f91dd3bcc5cb0dfda9f8d7674142d198fa640
max <maxime@leroymeunier.fr>
2025-01-06 21:49:33 +0100

    First Push

--- src/main/java/org/maxmailservice/Main.java
+++ src/main/java/org/maxmailservice/Main.java
@@ -2,7 +2,7 @@ package org.maxmailservice;
 
 public class Main {
     public static void main(String[] args) {
-        int port = 53245; // Port suggéré
+        int port = 53245; // Port par défaut
         Server server = new Server(port);
         server.start();
     }
